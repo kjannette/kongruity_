bdc88f5d16eb2ebcb90734a05d09feba521d2b78
KS Jannette <kjannette@KSs-MacBook-Air.local>
2026-02-11 14:12:05 -0500

    building out frontend

---
+++ frontend/resias/src/api/api.tsx
@@ -0,0 +1,1 @@
+import type { Sticky } from '../types/types';
--- frontend/resias/src/components/stickies.tsx
+++ frontend/resias/src/components/stickies.tsx
@@ -1,19 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
-import type { Sticky } from '../types/types';
-
-const fetchStickies = async (): Promise<Sticky[]> => {
-    const response = await fetch('http://localhost:3001/api/notes');
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  };
+import { useState} from "react";  
 
   const Stickies = () => {
-    const { data, isLoading, error } = useQuery({
-        queryKey: ['stickies'],
-        queryFn: fetchStickies,
-    });
+    const isLoading =  useState(false);
+    const error = useState(false);
+    let data: any = [];
 
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error: {error.message}</div>;
